5c42bd1fcfc0718cfdb2174fa873c9e9e3630db8
JacksonZZS <jacksonzhang1221@gmail.com>
2024-11-04 14:44:51 +0800

    test

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+print(1)
 
 class LoanApplicationApp:
     def __init__(self, root):
